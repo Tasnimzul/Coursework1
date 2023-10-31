18a6cd9a7ba75e2b79c061053b32635de3bf9cfd
Tasnimzul <146707516+Tasnimzul@users.noreply.github.com>
2023-10-31 17:48:33 +0000

    First commit coursework

---
+++ task1.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {       
+        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    FILE *file = fopen("FitnessData_2023.csv", "r");
+    if (file==NULL){
+        perror("");
+        return 1;
+    }
+    char data_line[];
+    int line = 0;
+    while (fgets(data_line, sizeof(data_line), file) != NULL){
+        line++;
+    }
+    printf("Number of records in file:%d\n", line);
+    while (fgets(data_line, sizeof(data_line), file) line<3){
+        
+    }
+
+
+
+}
