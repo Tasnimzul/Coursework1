ebf5d8690f0565f198129c9c9b7a898e03fbbb18
Tasnimzul <146707516+Tasnimzul@users.noreply.github.com>
2023-11-28 04:10:58 +0000

    coursework p2 n p3

--- FitnessDataSorter.c
+++ FitnessDataSorter.c
@@ -35,6 +35,41 @@ int main() {
          printf("Error: Could not find or open the file.\n");
          return 1;
          }
-    
-    
+    int data_size = 1000;
+    char data_line[data_size];
+    FitnessData stepcount[1000];
+    int line_count = 0;
+
+    while(fgets(data_line, data_size, file)!= NULL){
+        char date_token[11];
+        char time_token[6];
+        int steps_token;
+        tokeniseRecord(data_line, ',', date_token, time_token, &steps_token);
+        strcpy(stepcount[line_count].date, date_token);
+        strcpy(stepcount[line_count].time, time_token);
+        stepcount[line_count].steps = steps_token;
+
+        line_count++;
+        }
+    fclose(file);
+
+    for(int i = 0; i < line_count; i++){
+        for(int j = 0; j < line_count; j++){
+            if(stepcount[j].steps < stepcount[j+1].steps){
+                FitnessData data = stepcount[j];
+                stepcount[j] = stepcount[j+1];
+                stepcount[j+1] = data;
+                // sources : https://www.sanfoundry.com/c-program-sort-array-descending-order/
+            }
+        }
+    }
+
+    strcat(filename, ".tsv");
+    FILE *sortedfile = fopen(filename, "w");
+
+    for (int i=0; i<line_count; i++){
+        fprintf(sortedfile, "%s\t%s\t%d\n", stepcount[i].date, stepcount[i].time, stepcount[i].steps);
+    }
+    fclose(sortedfile);
+    return 0;
 }
