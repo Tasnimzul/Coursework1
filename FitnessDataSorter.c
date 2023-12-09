bbd7c869a4b84d25aaa39a98e22a9ef60068cd16
Tasnimzul <146707516+Tasnimzul@users.noreply.github.com>
2023-12-09 05:57:01 +0000

    Final

--- FitnessDataSorter.c
+++ FitnessDataSorter.c
@@ -25,61 +25,50 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
     }
 }
 
+int ordering(const void *x, const void *y){
+    return(*((FitnessData *)y)).steps - (*((FitnessData *)x)).steps;
+}
+
 
 
 int main() {
-    char filename[100];
+    char filename[1000];
     printf("Input filename:");
     scanf("%s", filename);
     FILE *file = fopen(filename, "r");
     if (file == NULL){
-         printf("Error: Could not find or open the file.\n");
+         printf("Error: invalid file\n");
          return 1;
          }
     int data_size = 1500;
     char data_line[data_size];
     FitnessData stepcount[1500];
+
+
     int line_count = 0;
-
-    if (fgets(data_line, data_size, file)== NULL){
-        printf("Error: invalid file");
-        return 1;
-        fclose(file);
-    }
-
-
     while(fgets(data_line, data_size, file)!= NULL){
-        line_count++;
         char date_token[11];
         char time_token[6];
         int steps_token;
         tokeniseRecord(data_line, ',', date_token, time_token, &steps_token);
-        if (strlen(date_token) != 10||strlen(time_token) != 5||&steps_token < 0){
+        if (strlen(date_token) != 10||strlen(time_token) != 5|| steps_token < 0){
         printf("Error: invalid file\n");
         return 1;
         }
         strcpy(stepcount[line_count].date, date_token);
         strcpy(stepcount[line_count].time, time_token);
         stepcount[line_count].steps = steps_token;
+        line_count++;
 
         }
     fclose(file);
 
-    for(int i = 0; i < line_count - 1; i++){
-        for(int j = i+1 ; j < line_count; j++){
-            if(stepcount[j].steps < stepcount[j+1].steps){
-                FitnessData data = stepcount[j];
-                stepcount[j] = stepcount[j+1];
-                stepcount[j+1] = data;
-                // sources : https://www.sanfoundry.com/c-program-sort-array-descending-order/
-            }
-        }
-    }
+    qsort(stepcount, line_count, sizeof(FitnessData), ordering);
 
     strcat(filename, ".tsv");
     FILE *sortedfile = fopen(filename, "w");
 
-    for (int i=0; i< line_count; i++){
+    for (int i=0; i < line_count; i++){
         fprintf(sortedfile, "%s\t%s\t%d\n", stepcount[i].date, stepcount[i].time, stepcount[i].steps);
     }
     fclose(sortedfile);
